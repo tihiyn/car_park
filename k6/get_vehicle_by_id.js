0923b848fad5c229fcd190205e91a5962d71f362
Tikhomirov Nikita <113970760+tihiyn@users.noreply.github.com>
2026-05-24 12:13:42 +0300

    Merge pull request #63 from tihiyn/Task-38
    [Task-38] Read JWT from env

--- k6/get_vehicle_by_id.js
+++ k6/get_vehicle_by_id.js
@@ -3,16 +3,21 @@ import { check } from 'k6';
 
 export const options = {
     stages: [
-        { duration: '30s', target: 10 },
-        { duration: '1m',  target: 10 },
+        { duration: '30s', target: 200 },
+        { duration: '1m',  target: 200 },
         { duration: '15s', target: 0 },
     ],
 };
 
 export default function () {
+    const token = __ENV.JWT;
     const res = http.get('http://nginx/api/vehicles/25024', {
-        headers: { Cookie: 'JWT=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiLQkNC90LjRgdC40LzQvtCy0JLQoSIsImlhdCI6MTc3ODUwNzgyOCwiZXhwIjoxNzc4NTUxMDI4fQ.4gwUplsT2fa4D6soaitLvhYVQB9uuZHJRB_-sfyplRk' },
+        headers: { Cookie: token },
     });
+
+    if (res.status !== 200) {
+        console.log(`Error: status=${res.status} body=${res.body}`);
+    }
 
     check(res, {
         'status 200': (r) => r.status === 200,
